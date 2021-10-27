273c8c3f9f30948d5a51407979b5215d4ca4a047
het <gocks0922@gmail.com>
2021-10-27 21:07:24 +0900

    Add quarter and score section

--- src/Page/Court/index.js
+++ src/Page/Court/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import View from './view';
 
 export default class Conatiner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quarter: 1,
+      homeName: 'HOME',
+      awayName: 'AWAY',
+      homeScore: 0,
+      awayScore: 0
+    };
+  }
+
   render() {
-    return <View />;
+    return <View {...this.state} />;
   }
 }
--- src/Page/Court/view.js
+++ src/Page/Court/view.js
@@ -1,7 +1,21 @@
 import './index.scss';
 
 function View(props) {
-  return <div className="court">court</div>;
+  return (
+    <div className="court">
+      <section className="court__section court__section--qas">
+        <h2 className="quarter">Q{props.quarter}</h2>
+        <section className="team team--home">
+          <h3 className="team__name">{props.homeName}</h3>
+          <span className="team__score">{props.homeScore}</span>
+        </section>
+        <section className="team team--away">
+          <h3 className="team__name">{props.awayName}</h3>
+          <span className="team__score">{props.awayScore}</span>
+        </section>
+      </section>
+    </div>
+  );
 }
 
 export default View;
