ce9173bee4e7d1d10ad2383359a0993c3fd4cf3f
het <gocks0922@gmail.com>
2021-11-15 17:45:41 +0900

    Markup, style header

---
+++ src/Asset/index.js
@@ -0,0 +1,4 @@
+import menuIcon from './Image/menu-icon.png';
+import editIcon from './Image/edit-icon.png';
+
+export { menuIcon, editIcon };
--- src/Component/Layout/index.js
+++ src/Component/Layout/index.js
@@ -3,7 +3,11 @@ import './index.scss';
 function Layout(props) {
   return (
     <>
-      <header></header>
+      <header>
+        <button className="header-btn header-btn--menu" />
+        <h1 className="header-title">NBA FINAL - GAME 1</h1>
+        <button className="header-btn header-btn--edit" />
+      </header>
       <main>{props.children}</main>
       <footer></footer>
     </>
