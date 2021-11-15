95208bc515860dab5f9a2a1e6fbb149e8017bd01
het <gocks0922@gmail.com>
2021-11-15 18:41:20 +0900

    Add nav

--- src/Component/Layout/index.js
+++ src/Component/Layout/index.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import { URL } from '../../Util/constant';
 import './index.scss';
 
 function Layout(props) {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <>
       <header>
-        <button className="header-btn header-btn--menu" />
+        <button
+          className="header-btn header-btn--menu"
+          onClick={(e) => setIsNavOpen((prev) => !prev)}
+        />
         <h1 className="header-title">NBA FINAL - GAME 1</h1>
         <button className="header-btn header-btn--edit" />
       </header>
@@ -25,6 +30,13 @@ function Layout(props) {
           <li className="footer-menu">Report</li>
         </ul>
       </footer>
+      <div
+        className="nav-wrapper"
+        nav-open={isNavOpen.toString()}
+        onClick={(e) => setIsNavOpen(false)}
+      >
+        <nav onClick={(e) => e.stopPropagation()}></nav>
+      </div>
     </>
   );
 }
